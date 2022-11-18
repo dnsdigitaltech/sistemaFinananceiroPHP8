a7439f1e2e3b24ddbdd810636bf8264fb66dbae4
Davi Bernardo <davi.bernardo@sbahq.org>
2022-11-18 14:15:41 -0300

    Imagem no Login

--- index.php
+++ index.php
@@ -6,23 +6,21 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- CSS only -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
-        <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
-        <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+        <!-- JavaScript Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
         <link href="css/estilo-login.css" rel="stylesheet">
     </head>
     <body>
         <div class="container">
             <div class="row">
-                <div class="">
-                    <div class="account-wall">
-                        <img class="profile-img" src="https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=120"
-                            alt="">
-                        <form class="form-signin" method="POST" action="atenticar.php">
-                        <input type="email" class="form-control" placeholder="E-mail" required autofocus>
-                        <input type="password" class="form-control" placeholder="Senha" required>
-                        <button class="btn btn-lg btn-primary btn-block" type="submit"> Entrar</button>                        
-                        </form>
-                    </div>
+                <div class="account-wall">
+                    <img class="profile-img" src="./img/logo.png"
+                        alt="">
+                    <form class="form-signin" method="POST" action="atenticar.php">
+                    <input type="email" class="form-control mb-2" placeholder="E-mail" required autofocus>
+                    <input type="password" class="form-control mb-2" placeholder="Senha" required>
+                    <button class="btn btn-lg btn-primary btn-block" type="submit"> Entrar</button>                        
+                    </form>
                 </div>
             </div>
         </div>
