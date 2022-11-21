3e9ea5d8eb44a694b7989ea67b42ae5836a540d3
Davi Bernardo <davi.bernardo@sbahq.org>
2022-11-21 19:22:17 -0300

    Aplicando DataTables

--- painel-adm/index.php
+++ painel-adm/index.php
@@ -33,6 +33,8 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="../DataTables/datatables.min.css"/>
+        <script type="text/javascript" src="../DataTables/datatables.min.js"></script>
     </head>
     <body>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
--- painel-adm/niveis.php
+++ painel-adm/niveis.php
@@ -6,3 +6,27 @@
 <div class="col-md-12 mt-3">
     <a type="button" class="btn btn-dark">Novo Nível</a>    
 </div>
+
+<table id="example" class="table table-hover my-4" style="width:100%">
+				<thead>
+					<tr>
+						<th>Status</th>
+						<th>Valor</th>
+						<th>Data</th>
+						<th>Hora</th>
+						<th>Operador</th>
+						<th>Pagamento</th>						
+						<th>Ações</th>
+					</tr>
+				</thead>
+				<tbody>
+</table>
+
+
+<script type="text/javascript">
+		$(document).ready(function() {
+			$('#example').DataTable({
+				"ordering": false
+			});
+		} );
+</script>
