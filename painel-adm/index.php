74ce2d6418ee3d2e781d9deef1a300b309c003bb
Davi Bernardo <davi.bernardo@sbahq.org>
2022-11-21 14:29:00 -0300

    Executando Ajax

--- painel-adm/index.php
+++ painel-adm/index.php
@@ -19,7 +19,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-        <link href="css/style.css" rel="stylesheet">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     </head>
     <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -84,27 +84,28 @@
         <h1 class="modal-title fs-5" id="exampleModalLabel">Editar Dados</h1>
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
-      <div class="modal-body">
-        <form id="form-perfil">
-            <div class="mb-3">
-                <label for="nome" class="form-label">Nome</label>
-                <input type="text" name="nome" class="form-control" id="nome" value="<?=$nomeUsario?>" placeholder="Nome">
+
+      <form id="form-perfil">
+        <div class="modal-body">
+                <div class="mb-3">
+                    <label for="nome" class="form-label">Nome</label>
+                    <input type="text" name="nome" class="form-control" id="nome" value="<?=$nomeUsario?>" placeholder="Nome">
+                </div>
+                <div class="mb-3">
+                    <label for="email" class="form-label">Email</label>
+                    <input type="email" name="email" class="form-control" id="email" value="<?=$emailUsario?>" placeholder="E-mail">
+                </div>
+                <div class="mb-3">
+                    <label for="senha" class="form-label">Senha</label>
+                    <input type="text" name="senha" class="form-control" id="senha" value="<?=$senhaUsario?>" placeholder="Senha">
+                </div>
+                <small><div id="mensagem-perfil" align="center"></div></small>             
             </div>
-            <div class="mb-3">
-                <label for="email" class="form-label">Email</label>
-                <input type="email" name="email" class="form-control" id="email" value="<?=$emailUsario?>" placeholder="E-mail">
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" id="btn-fechar-perfil">Cancelar</button>
+                <button type="submit" class="btn btn-primary">Editar</button>
             </div>
-            <div class="mb-3">
-                <label for="senha" class="form-label">Senha</label>
-                <input type="text" name="senha" class="form-control" id="senha" value="<?=$senhaUsario?>" placeholder="Senha">
-            </div>
-            <small><div class="mensagem-perfil" align="center"></div></small> 
         </form>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" id="btn-fechar-perfil">Cancelar</button>
-        <button type="button" class="btn btn-primary">Editar</button>
-      </div>
     </div>
   </div>
 </div>
@@ -114,7 +115,6 @@
 	$("#form-perfil").submit(function () {
 		event.preventDefault();
 		var formData = new FormData(this);
-
 		$.ajax({
 			url: "editar-perfil.php",
 			type: 'POST',
@@ -126,10 +126,9 @@
                     //$('#nome').val('');
                     //$('#cpf').val('');
                     $('#btn-fechar-perfil').click();
-                    window.location = "index.php?pagina="+pag;
+                    window.location = "index.php?";
 
                 } else {
-
                 	$('#mensagem-perfil').addClass('text-danger')
                 }
 
