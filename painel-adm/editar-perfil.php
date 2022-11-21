5d8919e16e95dcdf5fee2ca5fb9011ec8b3380d1
Davi Bernardo <davi.bernardo@sbahq.org>
2022-11-21 15:15:33 -0300

    Validação de Email Duplicado

--- painel-adm/editar-perfil.php
+++ painel-adm/editar-perfil.php
@@ -1,12 +1,23 @@
 <?php
+    require_once("../conexao.php");
     $nome   = $_POST['nome_usario'];
     $email  = $_POST['email_usario'];
     $senha  = $_POST['senha_usario'];
     $id     = $_POST['id_usario'];
 
-    $query = $pdo->prepare("UPDATE usuarios SET nome :nome, email :email, senha :senha WHERE id = $id");
+    //VALIDAR EMAIL
+    $query = $pdo->query("SELECT * FROM usuarios WHERE email = '$email'");
+    $res = $query->fetch(PDO::FETCH_ASSOC);
+    $totalReg = count($res);
+    if($totalReg > 0){
+        echo 'Este email já está cadastrado, escolha outro email!';
+        exit();
+    }
+
+    $query = $pdo->prepare("UPDATE usuarios SET nome = :nome, email = :email, senha = :senha WHERE id = $id");
             $query->bindValue(":nome", $nome);        
             $query->bindValue(":email", $email);
             $query->bindValue(":senha", $senha);
             $query->execute();
+    echo 'Salvo com Sucesso!';
 ?>
