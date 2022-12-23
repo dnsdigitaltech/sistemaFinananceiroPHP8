48d43ed9814d28f4e9946aa08c6a904a758eeb92
Davi Bernardo <davi.bernardo@sbahq.org>
2022-12-23 16:51:18 -0300

    Modal para Inserir Dados

--- painel-adm/niveis.php
+++ painel-adm/niveis.php
@@ -4,7 +4,7 @@
     $pagina = 'niveis';
 ?>
 <div class="col-md-12 my-3">
-    <a type="button" class="btn btn-dark">Novo Nível</a>    
+    <a data-bs-toggle="modal" data-bs-target="#modalForm" type="button" class="btn btn-dark">Novo Nível</a>    
 </div>
 <div class="tabela bg-light">
     <table id="example" class="table table-striped table-hover my-4 bg-light">
@@ -34,6 +34,34 @@
                 <?php }?>
         </tbody>
     </table>
+</div>
+
+<!-- Modal Form-->
+<div class="modal fade" id="modalForm" tabindex="-1" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h1 class="modal-title fs-5"><span id="tituloModal">Inserir Dados</span></h1>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+
+      <form id="form-perfil">
+        <div class="modal-body">
+                <div class="mb-3">
+                    <label for="nome" class="form-label">Nível</label>
+                    <input type="text" name="nivel" class="form-control" id="nivel" placeholder="Nível do Usuário">
+                </div>
+                
+                <input type="text" name="id" id="id">
+                <small><div id="mensagem" align="center"></div></small>             
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" id="btn-fechar-perfil">Cancelar</button>
+                <button type="submit" class="btn btn-primary">Editar</button>
+            </div>
+        </form>
+    </div>
+  </div>
 </div>
 
 <script type="text/javascript">
